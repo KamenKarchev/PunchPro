466c3f57853d87ee68c5056ee3ab7a47178cc357
Kamen Karchev <102304265+KamenKarchev@users.noreply.github.com>
2025-05-04 22:48:56 +0300

    white and black theam

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { UserProvider } from './contexts/UserContext';
+import { ThemeProvider } from './contexts/ThemeContext';
 import LoginScreen from './screens/LoginScreen';
 import ClockScreen from './screens/ClockScreen';
 import AnalyticsScreen from './screens/AnalyticsScreen';
@@ -11,28 +12,30 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <PaperProvider>
-      <UserProvider>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen 
-              name="Login" 
-              component={LoginScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen 
-              name="Clock" 
-              component={ClockScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen 
-              name="Analytics" 
-              component={AnalyticsScreen}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </UserProvider>
-    </PaperProvider>
+    <ThemeProvider>
+      <PaperProvider>
+        <UserProvider>
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+              <Stack.Screen 
+                name="Login" 
+                component={LoginScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen 
+                name="Clock" 
+                component={ClockScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen 
+                name="Analytics" 
+                component={AnalyticsScreen}
+                options={{ headerShown: false }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </UserProvider>
+      </PaperProvider>
+    </ThemeProvider>
   );
 }
